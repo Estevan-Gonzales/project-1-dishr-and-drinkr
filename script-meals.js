2f6c88e26e196aeb7ffe68cb69811501052cfc79
Estevan <egonz94@gmail.com>
2023-10-10 18:27:45 -0500

    Added meal search capability with basic Bulma widgets

---
+++ script-meals.js
@@ -0,0 +1,71 @@
+
+var ingredientListOneEl = document.getElementById('ingredient-list-one');
+var ingredientListTwoEl = document.getElementById('ingredient-list-two');
+var ingredientListThreeEl = document.getElementById('ingredient-list-three');
+var buttonSearchEl = document.getElementById('btn-search');
+
+var mealListEl = document.getElementById('meal-list');
+
+function populateIngredientsLists() {
+    var requestUrl = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+    localStorage.clear();
+  // replace `octocat` with anyone else's GitHub username
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        for(i = 0; i < data.meals.length; i++) {
+            localStorage.setItem(i, data.meals[i].strIngredient);
+            var listItemOne = document.createElement('option');
+            listItemOne.textContent = data.meals[i].strIngredient;
+            ingredientListOneEl.appendChild(listItemOne);
+            var listItemTwo = document.createElement('option');
+            listItemTwo.textContent = data.meals[i].strIngredient;
+            ingredientListTwoEl.appendChild(listItemTwo);
+            var listItemThree = document.createElement('option');
+            listItemThree.textContent = data.meals[i].strIngredient;
+            ingredientListThreeEl.appendChild(listItemThree);
+        }
+    });
+}
+
+function getMeals(event) {
+
+    event.preventDefault();
+    var baseUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='
+
+    var ingredientOne = ingredientListOneEl.value;
+
+    requestUrl = baseUrl + ingredientOne;
+
+    //localStorage.clear();
+  // replace `octocat` with anyone else's GitHub username
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+
+        if (data.meals.length == 0) {
+            var listItem = document.createElement('div');
+            listItem.textContent = "Sorry, no meals found :("
+            mealListEl.append(listItem);
+        }
+
+        for(i = 0; i < data.meals.length; i++) {
+            //localStorage.setItem(i, data.meals[i].strMeal);
+            var listItem = document.createElement('div');
+            //listItem.textContent = data.meals[i].strMeal;
+            listItem.textContent = data.meals[i].strMeal;
+            mealListEl.appendChild(listItem);
+    
+        }
+    });
+}
+
+populateIngredientsLists();
+
+buttonSearchEl.addEventListener('click', getMeals);
